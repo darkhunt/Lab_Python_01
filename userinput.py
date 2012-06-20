42dd5459d3d18e4fe6f7f76e2b2efb4ee64129c2
Darkhunt <aiti-mit@aitimit-VGN-NS160D.(none)>
2012-06-20 14:19:06 +0000

    lab01asnwers

---
+++ userinput.py
@@ -0,0 +1,7 @@
+first_name = raw_input("Enter your first name:")
+last_name= raw_input("Enter your last name:")
+print "Enter your date of birth:"
+month=raw_input("month?")
+D=raw_input("day?")
+Y=raw_input("year?")
+print first_name,"",last_name,"was born on ",month, D, Y 
